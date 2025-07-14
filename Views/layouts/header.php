33899026657f845fff98322c092b053912d137e7
Cruz Antonio <acg.purullena@gmail.com>
2025-07-14 13:28:32 +0200

    logo linked to homepage

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -1,11 +1,10 @@
 <header class="fade-in-down-initial">
     <div class="container header-content">
         <div class="logo">
-            <img src="/dashboard/Crusertel/assets/img/logo/imagen_logo_crusertel.jpg" alt="Logo Crusertel">
+            <a href="index.php?page=home"><img src="/dashboard/Crusertel/assets/img/logo/imagen_logo_crusertel.jpg" alt="Logo Crusertel"></a>
         </div>
         <nav>
             <ul>
-                <li><a href="index.php?page=home">Inicio</a></li>
                 <li><a href="index.php?page=services">Servicios</a></li>
                 <li><a href="index.php?page=faq">Preguntas Frecuentes</a></li>
                 <li><a href="index.php?page=contact">Contacto</a></li>
